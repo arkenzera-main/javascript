9597d0e8f2162c16cf1276ff78ff0899d327322e
Bruno P. Romero <brunopoliti8@gmail.com>
2024-06-27 21:49:50 -0300

    new file:   exercicios/Calculadora.js

---
+++ exercicios/Calculadora.js
@@ -0,0 +1,16 @@
+/**
+ * @author Bruno P. R.
+ * Calculadora gamer
+ * 
+ */
+
+const input = require('readline-sync')
+
+let bi1,bi2,bi3,bi4,mediafinal
+console.clear()
+bi1 = Number(input.question("Insira sua nota do primeiro bimestre:"))
+bi2 = Number(input.question("Insira sua nota do segundo bimestre:"))
+bi3 = Number(input.question("Insira sua nota do terceiro bimestre:"))
+bi4 = Number(input.question("Insira sua nota do quarto bimestre:"))
+mediafinal = (bi1+bi2+bi3+bi4)/4
+console.log(`Sua média final é ${bi1} + ${bi2} + ${bi3} + ${bi4} divido por 4, o resultado é ${mediafinal}`)
